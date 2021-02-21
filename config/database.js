67a5e81f6564adcd8fae6e3dcddede427959f18d
Chukwu Matthew <matthewnfvcb@gmail.com>
2021-02-21 22:28:11 +0100

    updated

--- config/database.js
+++ config/database.js
@@ -1,20 +1,20 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        host: env('DATABASE_HOST', 'pslc20.uuv5o.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'pslc20'),
-        username: env('DATABASE_USERNAME', 'pslc20'),
-        password: env('DATABASE_PASSWORD', 'pslc20'),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
+    defaultConnection: 'default',
+    connections: {
+        default: {
+            connector: 'mongoose',
+            settings: {
+                host: env('DATABASE_HOST', ),
+                srv: env.bool('DATABASE_SRV', true),
+                port: env.int('DATABASE_PORT', 27017),
+                database: env('DATABASE_NAME', ),
+                username: env('DATABASE_USERNAME', ),
+                password: env('DATABASE_PASSWORD', ),
+            },
+            options: {
+                authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+                ssl: env.bool('DATABASE_SSL', true),
+            },
+        },
     },
-  },
 });
